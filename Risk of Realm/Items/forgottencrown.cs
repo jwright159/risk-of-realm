334d2b75ac9c83eb4b2cdb57c893b9a79db83d5b
Zathsu <112720675+Zathsu@users.noreply.github.com>
2022-09-02 20:35:47 -0500

    Add files via upload

--- Risk of Realm/Items/forgottencrown.cs
+++ Risk of Realm/Items/forgottencrown.cs
@@ -13,7 +13,7 @@ namespace RiskOfRealm.Items
 		public override string ItemFullDescription => $"Killing an enemy raises all stats by {statsPerKill}%, up to a maximum of {statsKillCap}% <style=cStack>(+{statsKillCapPerStack}% per stack)</style>. All stats decay by {statsDecay}% <style=cStack>(+{statsDecayPerStack})% per stack</style>, after not killing enemies for <style=cIsUtility>{gracePeriod}</style> seconds.";
 		public override string ItemLore =>
 			"<style=cMono>\"An eerie reminder of what lays in the depths of...\"</style>\n" +
-			"The rest of the cracked old tablet was chipped. He could only wonder."
+			"The rest of the cracked old tablet was chipped. He could only wonder.";
 
 		public override ItemTier Tier => ItemTier.TierLunar;
 		public override ItemTag[] ItemTags { get; } = { ItemTag.Lunar };
